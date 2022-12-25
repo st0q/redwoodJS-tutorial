7439537526a3ca07f15277849d2a1cd334430d42
st0q <tssk713@gmail.com>
2022-12-25 17:50:01 +0900

    add SubmitHandler

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,12 +1,20 @@
-import { Form, TextField, Submit } from '@redwoodjs/forms'
+import { Form, TextField, Submit, SubmitHandler } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
+interface FormValues {
+  input: string
+}
+
 const ContactPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <TextField name="input" />
         <Submit>Save</Submit>
       </Form>
